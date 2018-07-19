0d513a84de179ca14cf6a749c62ff0a792bf9153
Perry Valdez <perry.valdez@ugroop.com>
2018-07-19 20:00:21 +0800

    Added signin page

---
+++ assets/js/signin.js
@@ -0,0 +1,7 @@
+function signin () {
+  alert('Signed In!');
+}
+
+var btnSignIn = document.getElementById('btn-signin');
+
+btnSignIn.addEventListener('click', signin);
--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 import home from './routes/home';
+import signin from './routes/siginin';
 
 const setup = (app) => {
   app.use('/', home);
+  app.use('/signin', signin);
 };
 
 export default { setup };
